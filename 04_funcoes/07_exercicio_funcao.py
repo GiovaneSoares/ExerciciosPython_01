e7e78405b96cf8a22f237e410ee0e829b21f1b92
Giovane Soares <soaresgeovane058@gmail.com>
2025-06-23 22:35:04 -0300

    Utilizando de POO para melhorar e enxugar meu algoritmo

--- 04_funcoes/07_exercicio_funcao.py
+++ 04_funcoes/07_exercicio_funcao.py
@@ -12,3 +12,33 @@ O programa deve continuar solicitando novos valores até que uma prestação inf
 seja zero, encerrando o programa e exibindo o relatório do dia com a quantidade e o
 valor total de prestações pagas.
 """
+
+def valor_pagamento(valor_prestacao, dias_atrasado):
+    if dias_atrasado > 0:
+        multa = valor_prestacao * 0.03
+        juros = dias_atrasado * 0.001 * valor_prestacao
+
+        return valor_prestacao + multa + juros
+    return valor_prestacao
+
+total_prestacao = 0
+valor_total_pago = 0
+
+while True:
+    valor_prestacao = int(input("Informe o valor da prestação. (ou 0 para sair): "))
+
+    if valor_prestacao == 0:
+        break
+
+dias_atraso = int(input("Informe os número de dias em atrasos "))
+
+valor_a_pagar = valor_pagamento(valor_prestacao, dias_atraso)
+
+print(f"Valor a pagar R${valor_a_pagar:.2f}")
+
+total_prestacao += 1
+valor_total_pago += valor_a_pagar
+
+print(f"    Relatorio do dia: ")
+print(f"Total das prestações: R${total_prestacao}")
+print(f"Valor total pago: R${valor_total_pago:.2f}")
